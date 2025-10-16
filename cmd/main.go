547a6f086be7e4f2784589721f551e7f6765401c
fantarqse <fantarose51@gmail.com>
2025-10-16 19:49:48 +0300

    feat(main): main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/fantarqse/cacheserver/internal/core/service"
+	"github.com/fantarqse/cacheserver/internal/httpserver"
+	"github.com/fantarqse/cacheserver/internal/storage"
+)
 
 func main() {
-	log.Fatalln("unimplemented")
+	// TODO: logger?
+	// TODO: config
+
+	storage := storage.New()
+	service := service.New(storage)
+	server := httpserver.New(service)
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
 }
