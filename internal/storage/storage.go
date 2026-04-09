7aa942bffba8cb1b79066d570432707020c904fd
fantarqse <fantarose51@gmail.com>
2026-04-09 22:11:23 +0300

    feat(storage): created basic functionality

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 
@@ -11,12 +12,13 @@ import (
 
 type storage struct {
 	rdb *redis.Client
+	ttl time.Duration
 }
 
 func New(ctx context.Context, rdb *redis.Client) (*storage, error) {
 	cmd := rdb.Ping(ctx)
 	if cmd.Err() != nil {
-		return nil, fmt.Errorf("failed to connect to redis: %w", cmd.Err()) // TODO: handle error correctly
+		return nil, fmt.Errorf("failed to connect to redis: %w", cmd.Err())
 	}
 
 	return &storage{
@@ -25,17 +27,35 @@ func New(ctx context.Context, rdb *redis.Client) (*storage, error) {
 }
 
 func (s *storage) Put(ctx context.Context, key string, page model.Page) error {
-	panic("unimplemented")
+	cmd := s.rdb.Set(ctx, key, page, s.ttl)
+	if cmd.Err() != nil {
+		return fmt.Errorf("failed to set value %s to the cache: %w", key, cmd.Err())
+	}
+
+	return nil
 }
 
 func (s *storage) Get(ctx context.Context, key string) (model.Page, error) {
-	panic("unimplemented")
+	cmd := s.rdb.Get(ctx, key)
+	if cmd.Err() != nil {
+		return model.Page{}, fmt.Errorf("failed to get value %s from the cache: %w", key, cmd.Err())
+	}
+
+	// TODO: convert value to model.Page
+	var _ = cmd
+	return model.Page{}, nil
 }
 
 func (s *storage) Delete(ctx context.Context, key string) error {
-	panic("unimplemented")
+	cmd := s.rdb.Del(ctx, key)
+	if cmd.Err() != nil {
+		return fmt.Errorf("failed to delete value %s from the cache: %w", key, cmd.Err())
+	}
+
+	return nil
 }
 
 func (s *storage) Top(ctx context.Context) ([]model.Page, error) {
+	// TODO: how to implement it?
 	panic("unimplemented")
 }
